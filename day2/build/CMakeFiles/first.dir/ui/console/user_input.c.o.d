cda1493fc43947b235923f8ae0a09f0c61a9405d
kkammong <jsw8718@gmail.com>
2023-08-22 16:11:36 +0900

    Day2 : delete build directory

--- day2/build/CMakeFiles/first.dir/ui/console/user_input.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/first.dir/ui/console/user_input.c.o: \
- /home/owen/THEONETHING/day2/ui/console/user_input.c \
- /usr/include/stdc-predef.h \
- /home/owen/THEONETHING/day2/ui/console/user_input.h
